abb6a4f5e143a9dafdf2bfd2f8bfd8179bd2b3d0
tochi <wingame546@gmail.com>
2025-01-09 23:37:23 +0900

    table情報変更

--- src/Table.tsx
+++ src/Table.tsx
@@ -1,95 +1,84 @@
-import React from 'react';
-import { useState,useEffect } from 'react';
-import{HorseIDData, BobaData, HinbaData} from './TaneData';
+import React, { useState, useEffect } from 'react';
+import { HorseIDData, BobaData, HinbaData } from './TaneData';
 
 const Table: React.FC = () => {
-    //データ
-    const [selectBoba, setSelectBoba] = useState<string>('');
-    const [selectHinba, setSelectHinba] = useState<string>('');
-    //const [boList, setbolist] = useState(HorseIDData);
-    const [horseList, setHorselist] = useState(HorseIDData);
+  const [selectBoba, setSelectBoba] = useState<string>('');
+  const [selectHinba, setSelectHinba] = useState<string>('');
+  const [horseList, setHorselist] = useState(HorseIDData);
 
-    
-    //牡馬変更時
-    const fetchBobaData = async(val:string)=>{
-        try{
-            setSelectBoba(val);
-            const selectedItem = BobaData.find(item => item.bobaId === selectBoba);
-            //console.log(selectedItem);
-            if(!selectedItem?.kumi) return;
-            const splitData = selectedItem.kumi.split('');
-            const HinbaIchiran = HorseIDData.filter(item => item.sex === 2);
-            
-            //console.log(HinbaIchiran);
-            let forCnt = 0;
-            splitData.forEach(el => {
-                if(el==="1") HinbaIchiran[forCnt].aisyo ="堅実";
-                if(el==="2") HinbaIchiran[forCnt].aisyo ="平均";
-                if(el==="3") HinbaIchiran[forCnt].aisyo ="一発";
-                forCnt++;
-            });
-            setHorselist(HinbaIchiran);
+  const fetchBobaData = async (val: string) => {
+    try {
+      setSelectBoba(val);
+      const selectedItem = BobaData.find(item => item.bobaId === val);
+      if (!selectedItem?.kumi) return;
 
-            const sortedData = [...HinbaIchiran].sort((a, b) => a.horseKeitou.localeCompare(b.horseKeitou));
-            setHorselist(sortedData);
-        }catch{
+      const splitData = selectedItem.kumi.split('');
+      const HinbaIchiran = HorseIDData.filter(item => item.sex === 2);
 
-        }
+      let forCnt = 0;
+      splitData.forEach(el => {
+        if (el === '1') HinbaIchiran[forCnt].aisyo = '堅実';
+        if (el === '2') HinbaIchiran[forCnt].aisyo = '平均';
+        if (el === '3') HinbaIchiran[forCnt].aisyo = '一発';
+        forCnt++;
+      });
+      setHorselist(HinbaIchiran);
+
+      const sortedData = [...HinbaIchiran].sort((a, b) => a.horseKeitou.localeCompare(b.horseKeitou));
+      setHorselist(sortedData);
+    } catch {
+      console.error('Error fetching Boba data');
     }
-    //牡馬セレクト発火
-    const bobaChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectHinba("B00");
-        setHorselist([]);
-        fetchBobaData(event.target.value);
-    };
+  };
 
-    useEffect(() => {
-        fetchBobaData(selectBoba);  // 非同期関数を呼び出し
-    }, [selectBoba]);
-    useEffect(() => {
-        fetchHinbaData(selectHinba);  // 非同期関数を呼び出し
-    }, [selectHinba]);
+  const bobaChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectHinba("B00");
+    setHorselist([]);
+    fetchBobaData(event.target.value);
+  };
 
-
-    //牝馬変更時
-    const fetchHinbaData = async(val:string)=>{
-        try{
-            setSelectHinba(val);
-            const selectedhinItem = HinbaData.find(item => item.hinbaId === selectHinba);
-            console.log(selectedhinItem);
-            if(!selectedhinItem?.Kumi) return;
-            const splitData = selectedhinItem.Kumi.split('');
-            const BobaIchiran = HorseIDData.filter(item => item.sex === 1);
-            
-            //console.log(BobaIchiran);
-            let forCnt = 0;
-            splitData.forEach(el => {
-                if(el==="1") BobaIchiran[forCnt].aisyo ="堅実";
-                if(el==="2") BobaIchiran[forCnt].aisyo ="平均";
-                if(el==="3") BobaIchiran[forCnt].aisyo ="一発";
-                forCnt++;
-            });
-            setHorselist(BobaIchiran);
-
-            const sortedData = [...BobaIchiran].sort((a, b) => a.horseKeitou.localeCompare(b.horseKeitou));
-            setHorselist(sortedData);
-        }catch{
-
-        }
+  useEffect(() => {
+    if (selectBoba) {
+      fetchBobaData(selectBoba);
     }
-    //牝馬セレクト発火
-    const hinbaChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectBoba("A00");
-        setHorselist([]);
-        fetchHinbaData(event.target.value);
-    };
+  }, [selectBoba]);
 
+  const fetchHinbaData = async (val: string) => {
+    try {
+      setSelectHinba(val);
+      const selectedHinItem = HinbaData.find(item => item.hinbaId === val);
+      if (!selectedHinItem?.Kumi) return;
 
-    return (
-      <div className="">
-        <h1>血統表</h1>
-        <label>牡馬血統：
-          <select name="boba" value={selectBoba} onChange={bobaChange}>
+      const splitData = selectedHinItem.Kumi.split('');
+      const BobaIchiran = HorseIDData.filter(item => item.sex === 1);
+
+      let forCnt = 0;
+      splitData.forEach(el => {
+        if (el === '1') BobaIchiran[forCnt].aisyo = '堅実';
+        if (el === '2') BobaIchiran[forCnt].aisyo = '平均';
+        if (el === '3') BobaIchiran[forCnt].aisyo = '一発';
+        forCnt++;
+      });
+      setHorselist(BobaIchiran);
+
+      const sortedData = [...BobaIchiran].sort((a, b) => a.horseKeitou.localeCompare(b.horseKeitou));
+      setHorselist(sortedData);
+    } catch {
+      console.error('Error fetching Hinba data');
+    }
+  };
+
+  const hinbaChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectBoba("A00");
+    setHorselist([]);
+    fetchHinbaData(event.target.value);
+  };
+
+  return (
+    <div className="">
+      <h1>血統表</h1>
+      <label>牡馬血統：
+        <select name="boba" value={selectBoba} onChange={bobaChange}>
           <option value="A00">-----選択-----</option>
           <option value="A01">不明</option>
           <option value="A02">ダーレーアラビアン系</option>
@@ -114,11 +103,11 @@ const Table: React.FC = () => {
           <option value="A21">マンノウォー系</option>
           <option value="A22">オルコックアラビアン系</option>
           <option value="A23">ダイオメド系</option>
-          </select>
-        </label>
-        <br/>
-        <label>牝馬血統：
-          <select name="hinba" value={selectHinba} onChange={hinbaChange}>
+        </select>
+      </label>
+      <br />
+      <label>牝馬血統：
+        <select name="hinba" value={selectHinba} onChange={hinbaChange}>
           <option value="B00">-----選択-----</option>
           <option value="B01">不明</option>
           <option value="B02">プロミス族</option>
@@ -152,39 +141,36 @@ const Table: React.FC = () => {
           <option value="B30">ピピンペグズダム族</option>
           <option value="B31">ミラ族</option>
           <option value="B32">マイナーファミリーライン</option>
-          </select>
-        </label>
-        <br/>
-        <label>結果：</label>
-
-        <div className="">
-          <table>
-            <thead>
-                <tr>
-                    <th style={{ border: '1px solid black', padding: '8px' }}>馬名</th>
-                    <th style={{ border: '1px solid black', padding: '8px' }}>相性</th>
-                </tr>
-            </thead>
-            <tbody>
-            {horseList.map((row) => (
-            <tr key={row.horseID}
-            style={{
-                backgroundColor:
-                  row.aisyo === "堅実" ? '#add6ff' : row.aisyo === "平均" ? '#ffff9e' : row.aisyo === "一発" ? '#ff9e9e' : '#ffffff', // idによる色付け
-              }}
-            >
-               <td style={{ border: '1px solid black', padding: '8px' }}>{row.horseKeitou}</td>
-               <td style={{ border: '1px solid black', padding: '8px' }}>{row.aisyo}</td>
+        </select>
+      </label>
+      <br />
+      <label>結果：</label>
+      <div className="">
+        <table>
+          <thead>
+            <tr>
+              <th style={{ border: '1px solid black', padding: '8px' }}>馬名</th>
+              <th style={{ border: '1px solid black', padding: '8px' }}>相性</th>
             </tr>
+          </thead>
+          <tbody>
+            {horseList.map((row) => (
+              <tr
+                key={row.horseID}
+                style={{
+                  backgroundColor:
+                    row.aisyo === '堅実' ? '#add6ff' : row.aisyo === '平均' ? '#ffff9e' : row.aisyo === '一発' ? '#ff9e9e' : '#ffffff', // idによる色付け
+                }}
+              >
+                <td style={{ border: '1px solid black', padding: '8px' }}>{row.horseKeitou}</td>
+                <td style={{ border: '1px solid black', padding: '8px' }}>{row.aisyo}</td>
+              </tr>
             ))}
-            </tbody>
-          </table>
-        </div>
+          </tbody>
+        </table>
       </div>
-      
+    </div>
+  );
+};
 
-        
-    );
-  };
-  
-  export default Table;
+export default Table;
