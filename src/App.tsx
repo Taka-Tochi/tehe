4efb32eda5073e6cf495e7157bc0dbbb0b8c35c7
tochi <wingame546@gmail.com>
2025-01-09 23:45:57 +0900

    test追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home'
 import Table from './Table'
+import Test from './Test'
 import Navbar from './components/Navbar';  // Navbarコンポーネントをインポート
 
 const App: React.FC = () => {
   return (
     <Router basename="/tehe">  {/* ここでbasenameを設定 */}
-      <Navbar />  {/* ナビゲーションバーを表示 */}
+      
       <Routes>
         <Route path="/table" element={<Table />} />
+        <Route path="/test" element={<Test />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
---
+++ src/Test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+ 
+const Test: React.FC = () => {
+    return <h2>Test Page</h2>;
+}
+ 
+export default Test;
